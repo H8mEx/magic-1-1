6b477049194e2620d4fe420d0f5cc5beba9baccb
Soeren-Weingarten <s.weingarten@fokusmedien.de>
2022-10-31 16:49:07 +0100

    Hotfix

--- commands/join.js
+++ commands/join.js
@@ -10,7 +10,15 @@ const dcs = "676060833150926858"
 const warthunder = ""
 const il2 = ""
 const gta = ""
+const rulesproof = "748100298144481290"
 
+const generalchat = client.channels.cache.get("257984126718574592")
+
+var chosenString1 = ""
+var chosenString2 = ""
+var chosenString3 = ""
+
+let usernameX = ""
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -61,10 +69,11 @@ module.exports = {
                 
          setTimeout(function() {
 
-            var chosenString1 = interaction.options.getString('origin');
-            var chosenString2 = interaction.options.getString('age');
-            var chosenString3 = interaction.options.getString('game');
-
+             chosenString1 = interaction.options.getString('origin');
+             chosenString2 = interaction.options.getString('age');
+             chosenString3 = interaction.options.getString('game');
+             usernameX = interaction.user.tag
+             console.log("New user: "+usernameX)
             console.log("A new member has joined from: "+chosenString1+". He/She is "+chosenString2+" years old and mainly flies in "+chosenString3+"." )
 
 
@@ -78,12 +87,44 @@ module.exports = {
                 gamerole = gta
                 
             } 
+
+            
             interaction.member.roles.add(gamerole);
-},400);
+            interaction.member.roles.add(rulesproof);
+
+},800);
 await interaction.member.roles.add("339703419537457175");
+
+setTimeout(function() {
+    createEmbed()
+    interaction.guild.channels.cache.get("257984126718574592").send({ embeds: [Embed]});
+// generalchat.send({ embeds: [Embed]});
+
+},2000);
 return interaction.reply("You are all set! Enjoy your stay!")
+}
+
 
 }
 
-}
+function createEmbed() {
+    Embed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle(usernameX.toString()+" has landed on RW01.")
+    .setURL('https://pcpilotscrew.com/')
+    .setAuthor({ name: 'PC Pilots Crew', iconURL: 'https://pcpilotscrew.com/wp-content/uploads/2022/02/pcpi-emblem-clean-small.png', url: 'https://discord.js.org' })
+    .setDescription("Come and say hello.")
+    .setThumbnail('https://pcpilotscrew.com/wp-content/uploads/photo-gallery/pcpi2_ytlogo_1024.png?bwg=1584451838')
+    .addFields(
 
+
+        { name: 'Location: ', value: chosenString1.toString(), inline: true },
+        { name: 'Age: ', value: chosenString2.toString(), inline: true },
+        { name: 'Main Game: ', value: chosenString3.toString(), inline: true },
+
+      
+    )
+    //.setImage(aircraft1.image)
+    .setTimestamp()
+    .setFooter({ text: 'pcpilotscrew.com', iconURL: 'https://pcpilotscrew.com/wp-content/uploads/2022/02/pcpi-emblem-clean-small.png' });
+    }
